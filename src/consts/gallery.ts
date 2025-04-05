346b31a47c63094273d1ea60515beff19319c755
Camilo <c.acevedo1993@gmail.com>
2025-04-05 12:06:55 -0500

    feat: added additional libraries and set width and height in project images

--- src/consts/gallery.ts
+++ src/consts/gallery.ts
@@ -3,286 +3,396 @@ export const GALLERY = {
         {
             id: 1,
             src: "/images/letras/1.webp",
-            alt: "description ----"
+            alt: "description ----",
+            width: 960,
+            height: 1280,
         },
         {
             id: 2,
             src: "/images/letras/2.webp",
-            alt: "description ----"
+            alt: "description ----",
+            width: 960,
+            height: 1280,
         },
         {
             id: 3,
             src: "/images/letras/3.webp",
-            alt: "description ----"
+            alt: "description ----",
+            width: 963,
+            height: 1280,
         },
         {
             id: 4,
             src: "/images/letras/4.webp",
-            alt: "description ----"
+            alt: "description ----",
+            width: 1280,
+            height: 847,
         },
         {
             id: 5,
             src: "/images/letras/5.webp",
-            alt: "description ----"
+            alt: "description ----",
+            width: 963,
+            height: 1280,
         },
         {
             id: 6,
             src: "/images/letras/6.webp",
-            alt: "description ----"
+            alt: "description ----",
+            width: 1280,
+            height: 852,
         },
         {
             id: 7,
             src: "/images/letras/7.webp",
-            alt: "description ----"
+            alt: "description ----",
+            width: 1280,
+            height: 852,
         },
         {
             id: 8,
             src: "/images/letras/8.webp",
-            alt: "description ----"
+            alt: "description ----",
+            width: 1280,
+            height: 960,
         },
         {
             id: 9,
             src: "/images/letras/9.webp",
-            alt: "description ----"
+            alt: "description ----",
+            width: 1280,
+            height: 853,
         },
         {
             id: 10,
             src: "/images/letras/10.webp",
-            alt: "description ----"
+            alt: "description ----",
+            width: 1280,
+            height: 853,
         },
         {
             id: 11,
             src: "/images/letras/11.webp",
-            alt: "description ----"
+            alt: "description ----",
+            width: 721,
+            height: 853,
         },
         {
             id: 12,
             src: "/images/letras/12.webp",
-            alt: "description ----"
+            alt: "description ----",
+            width: 1280,
+            height: 960,
         },
         {
             id: 13,
             src: "/images/letras/13.webp",
-            alt: "description ----"
+            alt: "description ----",
+            width: 960,
+            height: 1280,
         }
     ],
     mural: [
         {
             id: 1,
             src: "/images/mural/1.webp",
-            alt: "description ----"
+            alt: "description ----",
+            width: 1280,
+            height: 960,
         },
         {
             id: 2,
             src: "/images/mural/2.webp",
-            alt: "description ----"
+            alt: "description ----",
+            width: 1280,
+            height: 960,
         },
         {
             id: 3,
             src: "/images/mural/3.webp",
-            alt: "description ----"
+            alt: "description ----",
+            width: 1280,
+            height: 960,
         },
         {
             id: 4,
             src: "/images/mural/4.webp",
-            alt: "description ----"
+            alt: "description ----",
+            width: 960,
+            height: 1280,
         },
         {
             id: 5,
             src: "/images/mural/5.webp",
-            alt: "description ----"
+            alt: "description ----",
+            width: 960,
+            height: 1280,
         },
         {
             id: 6,
             src: "/images/mural/6.webp",
-            alt: "description ----"
+            alt: "description ----",
+            width: 960,
+            height: 1280,
         },
         {
             id: 7,
             src: "/images/mural/7.webp",
-            alt: "description ----"
+            alt: "description ----",
+            width: 960,
+            height: 1280,
         },
         {
             id: 8,
             src: "/images/mural/8.webp",
-            alt: "description ----"
+            alt: "description ----",
+            width: 1280,
+            height: 960,
         },
         {
             id: 9,
             src: "/images/mural/9.webp",
-            alt: "description ----"
+            alt: "description ----",
+            width: 960,
+            height: 1280,
         },
         {
             id: 10,
             src: "/images/mural/10.webp",
-            alt: "description ----"
+            alt: "description ----",
+            width: 960,
+            height: 1280,
         },
         {
             id: 11,
             src: "/images/mural/11.webp",
-            alt: "description ----"
+            alt: "description ----",
+            width: 960,
+            height: 1280,
         },
         {
             id: 12,
             src: "/images/mural/12.webp",
-            alt: "description ----"
+            alt: "description ----",
+            width: 1280,
+            height: 960,
         }
     ],
     nido: [
         {
             id: 1,
             src: "/images/nido/1.webp",
-            alt: "description ----"
+            alt: "description ----",
+            width: 960,
+            height: 1280,
         },
         {
             id: 2,
             src: "/images/nido/2.webp",
-            alt: "description ----"
+            alt: "description ----",
+            width: 960,
+            height: 1280,
         },
         {
             id: 3,
             src: "/images/nido/3.webp",
-            alt: "description ----"
+            alt: "description ----",
+            width: 1280,
+            height: 960,
         },
         {
             id: 4,
             src: "/images/nido/4.webp",
-            alt: "description ----"
+            alt: "description ----",
+            width: 960,
+            height: 1280,
         },
         {
             id: 5,
             src: "/images/nido/5.webp",
-            alt: "description ----"
+            alt: "description ----",
+            width: 900,
+            height: 1200,
         },
         {
             id: 6,
             src: "/images/nido/6.webp",
-            alt: "description ----"
+            alt: "description ----",
+            width: 1024,
+            height: 1024,
         },
         {
             id: 7,
             src: "/images/nido/7.webp",
-            alt: "description ----"
+            alt: "description ----",
+            width: 960,
+            height: 1280,
         }
     ],
     paz: [
         {
             id: 1,
             src: "/images/paz/1.webp",
-            alt: "description ----"
+            alt: "description ----",
+            width: 1280,
+            height: 853,
         },
         {
             id: 2,
             src: "/images/paz/2.webp",
-            alt: "description ----"
+            alt: "description ----",
+            width: 1280,
+            height: 738,
         },
         {
             id: 3,
             src: "/images/paz/3.webp",
-            alt: "description ----"
+            alt: "description ----",
+            width: 1280,
+            height: 853,
         },
         {
             id: 4,
             src: "/images/paz/4.webp",
-            alt: "description ----"
+            alt: "description ----",
+            width: 1280,
+            height: 853,
         }
         ,
         {
             id: 5,
             src: "/images/paz/5.webp",
-            alt: "description ----"
+            alt: "description ----",
+            width: 1280,
+            height: 853,
         },
     ],
     sororidad: [
         {
             id: 1,
             src: "/images/sororidad/1.webp",
-            alt: "description ----"
+            alt: "description ----",
+            width: 1280,
+            height: 853,
         },
         {
             id: 2,
             src: "/images/sororidad/2.webp",
-            alt: "description ----"
+            alt: "description ----",
+            width: 1280,
+            height: 853,
         },
         {
             id: 3,
             src: "/images/sororidad/3.webp",
-            alt: "description ----"
+            alt: "description ----",
+            width: 1280,
+            height: 853,
         },
         {
             id: 4,
             src: "/images/sororidad/4.webp",
-            alt: "description ----"
+            alt: "description ----",
+            width: 1280,
+            height: 853,
         },
         {
             id: 5,
             src: "/images/sororidad/5.webp",
-            alt: "description ----"
+            alt: "description ----",
+            width: 1280,
+            height: 853,
         },
         {
             id: 6,
             src: "/images/sororidad/6.webp",
-            alt: "description ----"
+            alt: "description ----",
+            width: 1280,
+            height: 853,
         },
         {
             id: 7,
             src: "/images/sororidad/7.webp",
-            alt: "description ----"
+            alt: "description ----",
+            width: 1280,
+            height: 853,
         },
         {
             id: 8,
             src: "/images/sororidad/8.webp",
-            alt: "description ----"
+            alt: "description ----",
+            width: 1280,
+            height: 853,
         },
         {
             id: 9,
             src: "/images/sororidad/9.webp",
-            alt: "description ----"
+            alt: "description ----",
+            width: 1280,
+            height: 853,
         },
         {
             id: 10,
             src: "/images/sororidad/10.webp",
-            alt: "description ----"
+            alt: "description ----",
+            width: 1280,
+            height: 853,
         },
         {
             id: 11,
             src: "/images/sororidad/11.webp",
-            alt: "description ----"
+            alt: "description ----",
+            width: 1280,
+            height: 853,
         },
         {
             id: 12,
             src: "/images/sororidad/12.webp",
-            alt: "description ----"
+            alt: "description ----",
+            width: 1280,
+            height: 853,
         },
         {
             id: 13,
             src: "/images/sororidad/13.webp",
-            alt: "description ----"
+            alt: "description ----",
+            width: 1280,
+            height: 853,
         },
         {
             id: 14,
             src: "/images/sororidad/14.webp",
-            alt: "description ----"
+            alt: "description ----",
+            width: 1280,
+            height: 853,
         },
         {
             id: 15,
             src: "/images/sororidad/15.webp",
-            alt: "description ----"
+            alt: "description ----",
+            width: 960,
+            height: 1280,
         },
         {
             id: 16,
             src: "/images/sororidad/16.webp",
-            alt: "description ----"
+            alt: "description ----",
+            width: 1280,
+            height: 960,
         },
         {
             id: 17,
             src: "/images/sororidad/17.webp",
-            alt: "description ----"
+            alt: "description ----",
+            width: 960,
+            height: 1280,
         },
         {
             id: 18,
             src: "/images/sororidad/18.webp",
-            alt: "description ----"
+            alt: "description ----",
+            width: 1280,
+            height: 960,
         }
     ],
 }
