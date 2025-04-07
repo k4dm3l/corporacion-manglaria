956581bfb06a5205f08ac98f0e28c0a2fb142452
DiegoContrerasA <diegocontreras1219@gmail.com>
2025-04-07 14:21:40 -0500

    feat: add nav

--- src/consts/links.ts
+++ src/consts/links.ts
@@ -16,8 +16,8 @@ export const FOTER_LINKS = [
     },
     {
         id: 4,
-        text: "¿Qué Hacemos?",
-        hash: "/#work",
+        text: "Galeria",
+        hash: "/gallery",
     },
     {
         id: 5,
@@ -36,7 +36,52 @@ export const FOTER_LINKS = [
     },
     {
         id: 8,
+        text: "¿Qué Hacemos?",
+        hash: "/#work",
+    },
+]
+
+
+
+export const LANDING_LINKS = [
+    {
+        id: 1,
+        text: "Inicio",
+        hash: "#home",
+    },
+    {
+        id: 2,
+        text: "Nosotros",
+        hash: "#about",
+    },
+    {
+        id: 3,
+        text: "Proyectos",
+        link: "#prjects",
+    },
+    {
+        id: 4,
+        text: "¿Qué Hacemos?",
+        hash: "#work",
+    },
+    {
+        id: 5,
+        text: "Blog",
+        link: "#blog",
+    },
+    {
+        id: 6,
+        text: "Impacto",
+        hash: "#numbers",
+    },
+    {
+        id: 7,
+        text: "Nuestro Equipo",
+        hash: "#team",
+    },
+    {
+        id: 8,
         text: "Galeria",
         hash: "/gallery",
-    }
+    },
 ]
