a40980b6f0a76b6c9f4df95573e4f7bbba05b1e4
DiegoContrerasA <diegocontreras1219@gmail.com>
2025-04-06 21:40:42 -0500

    feat: add footer links

---
+++ src/consts/links.ts
@@ -0,0 +1,42 @@
+export const FOTER_LINKS = [
+    {
+        id: 1,
+        text: "Inicio",
+        hash: "/#home",
+    },
+    {
+        id: 2,
+        text: "Sobre Nosotros",
+        hash: "/#about",
+    },
+    {
+        id: 3,
+        text: "Proyectos",
+        link: "/prjects",
+    },
+    {
+        id: 4,
+        text: "¿Qué Hacemos?",
+        hash: "/#work",
+    },
+    {
+        id: 5,
+        text: "Nuestro Blog",
+        link: "/blog",
+    },
+    {
+        id: 6,
+        text: "Nuestros Números",
+        hash: "/#numbers",
+    },
+    {
+        id: 7,
+        text: "Nuestro Equipo",
+        hash: "/#team",
+    },
+    {
+        id: 8,
+        text: "Galeria",
+        hash: "/gallery",
+    }
+]
