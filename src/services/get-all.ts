c6d31863ebcb5c0340b71cb9d2daf5dc855dc2d1
Camilo <c.acevedo1993@gmail.com>
2025-04-08 11:23:02 -0500

    adjust API call

--- src/services/get-all.ts
+++ src/services/get-all.ts
@@ -1,7 +1,7 @@
 import { fetcher } from '@/lib/fetcher';
 export const getAll = async ({ type, page = '1', limit = '10' }: { type: string, page: string, limit?: string }) => {
     try {
-        const data = await fetcher.get(`/${type}?page=${page}&limit=${limit}`).then(res => res.data)
+        const data = await fetcher.get(`/api/content/${type}/?page=${page}&limit=${limit}`).then(res => res.data)
         return { data, error: false }
     } catch (e) {
         console.log('GET ALL', { e })
--- src/services/get-by-slug.ts
+++ src/services/get-by-slug.ts
@@ -2,7 +2,7 @@ import { fetcher } from "@/lib/fetcher"
 
 export const getBydSlug = async ({ type, slug }: { type: string, slug: string }) => {
     try {
-        const data = await fetcher.get(`/${type}/${slug}`).then(res => res.data)
+        const data = await fetcher.get(`/api/content/${type}/${slug}`).then(res => res.data)
         return { data, error: false }
     } catch (e) {
         console.log('GET BY SLUG', {e})
