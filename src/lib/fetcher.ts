dce2625a41d070db678936f0cc373a1a8de90398
DiegoContrerasA <diegocontreras1219@gmail.com>
2025-06-18 15:03:40 -0500

    feat: add docs section

--- src/lib/fetcher.ts
+++ src/lib/fetcher.ts
@@ -1,10 +1,5 @@
 import axios from "axios";
 
-console.log({
-  baseURL: import.meta.env.PUBLIC_API_URL,
-  Authorization: `Bearer ${import.meta.env.PUBLIC_API_TOKEN}`,
-})
-
 export const fetcher = axios.create({
   baseURL: import.meta.env.PUBLIC_API_URL,
   timeout: 10000,
